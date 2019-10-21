581bb77e620f454c1099e0f1b08aaace90052e10
Gjoko <temelkovgjoko@gmail.com>
2019-10-22 00:18:58 +0200

    password hashing

--- models/User.js
+++ models/User.js
@@ -1,3 +1,4 @@
+const bcrypt = require("bcryptjs")
 const usersCollection = require('../db').collection("users")
 const validator = require("validator")
 
@@ -25,7 +26,7 @@ User.prototype.validate = function () {
     if (!validator.isEmail(this.data.email)) { this.errors.push("You must provide a valid email address.") }
     if (this.data.password == "") { this.errors.push("You must provide a password.") }
     if (this.data.password.length > 0 && this.data.password.length < 12) { this.errors.push("Password must be at least 12 characters.") }
-    if (this.data.password.length > 100) { this.errors.push("Password cannot exceed 100 characters.") }
+    if (this.data.password.length > 50) { this.errors.push("Password cannot exceed 50 characters.") }
     if (this.data.username.length > 0 && this.data.username.length < 3) { this.errors.push("Username must be at least 3 characters.") }
     if (this.data.username.length > 30) { this.errors.push("Username cannot exceed 30 characters.") }
 }
@@ -35,7 +36,7 @@ User.prototype.login = function () {
         this.cleanUp()
         usersCollection.findOne({ username: this.data.username }).then(
             attemptedUser => {
-                if (attemptedUser && attemptedUser.password == this.data.password) {
+                if (attemptedUser && bcrypt.compareSync(this.data.password, attemptedUser.password)) {
                     resolve("Congrats!")
                 } else {
                     reject("Invalid username / password")
@@ -55,6 +56,9 @@ User.prototype.register = function () {
     // Step #2: Only if there are no validation errors 
     // then save the user data into a database
     if (!this.errors.length) {
+        // hash user password
+        let salt = bcrypt.genSaltSync(10)
+        this.data.password = bcrypt.hashSync(this.data.password, salt)
         usersCollection.insertOne(this.data)
     }
 }
