6551e77ae89665d6ce10eee31125b6f1142825e0
Gjoko <temelkovgjoko@gmail.com>
2019-10-21 11:52:42 +0200

    first commit

---
+++ controllers/userController.js
@@ -0,0 +1,19 @@
+const User = require('../models/User')
+
+exports.login = function () {
+
+}
+
+exports.logout = function () {
+
+}
+
+exports.register = function (req, res) {
+    let user = new User(req.body)
+    user.register()
+    res.send("Thanks for trying to register")
+}
+
+exports.home = function (req, res) {
+    res.render('home-guest')
+}
