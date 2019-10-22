d8d6a31f3af30564708adf4905b831fb997655a8
Gjoko <temelkovgjoko@gmail.com>
2019-10-22 14:29:34 +0200

    adding user profile photos (gravatar)

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,7 +3,7 @@ const User = require('../models/User')
 exports.login = function (req, res) {
     let user = new User(req.body)
     user.login().then((result) => {
-        req.session.user = { favColor: "blue", username: user.data.username }
+        req.session.user = { avatar: user.avatar, username: user.data.username }
         req.session.save(() => {
             res.redirect('/')
         })
@@ -25,7 +25,7 @@ exports.logout = (req, res) => {
 exports.register = function (req, res) {
     let user = new User(req.body)
     user.register().then(() => {
-        req.session.user = { username: user.data.username }
+        req.session.user = { username: user.data.username, avatar: user.avatar }
         req.session.save(() => {
             res.redirect('/')
         })
@@ -42,7 +42,7 @@ exports.register = function (req, res) {
 
 exports.home = function (req, res) {
     if (req.session.user) {
-        res.render('home-dashboard', { username: req.session.user.username })
+        res.render('home-dashboard', { username: req.session.user.username, avatar: req.session.user.avatar })
     } else {
         res.render('home-guest', { errors: req.flash('errors'), regErrors: req.flash('regErrors') })
     }
--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
 const bcrypt = require("bcryptjs")
 const usersCollection = require('../db').db().collection("users")
 const validator = require("validator")
+const md5 = require('md5')
 
 let User = function (data) {
     this.data = data
@@ -20,7 +21,7 @@ User.prototype.cleanUp = function () {
     }
 }
 
-User.prototype.validate = function()  {
+User.prototype.validate = function () {
     return new Promise(async (resolve, reject) => {
         if (this.data.username == "") { this.errors.push("You must provide a username.") }
         if (this.data.username != "" && !validator.isAlphanumeric(this.data.username)) { this.errors.push("Username can only contain letters and numbers.") }
@@ -52,6 +53,8 @@ User.prototype.login = function () {
         usersCollection.findOne({ username: this.data.username }).then(
             attemptedUser => {
                 if (attemptedUser && bcrypt.compareSync(this.data.password, attemptedUser.password)) {
+                    this.data = attemptedUser
+                    this.getAvatar()
                     resolve("Congrats!")
                 } else {
                     reject("Invalid username / password")
@@ -63,7 +66,7 @@ User.prototype.login = function () {
     })
 }
 
-User.prototype.register = async function () {
+User.prototype.register = function () {
     return new Promise(async (resolve, reject) => {
         // Step #1: Validate user data
         this.cleanUp()
@@ -76,11 +79,16 @@ User.prototype.register = async function () {
             let salt = bcrypt.genSaltSync(10)
             this.data.password = bcrypt.hashSync(this.data.password, salt)
             await usersCollection.insertOne(this.data)
+            this.getAvatar()
             resolve()
         } else {
             reject(this.errors)
         }
     })
+}
+
+User.prototype.getAvatar = function () {
+    this.avatar = `https://gravatar.com/avatar/${md5(this.data.email)}?s=128`
 }
 
 module.exports = User
