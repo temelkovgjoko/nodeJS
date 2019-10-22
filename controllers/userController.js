648b99e390a1d1d062b28aa2d29d6de62d269f37
Gjoko <temelkovgjoko@gmail.com>
2019-10-22 16:25:09 +0200

    post model 2

--- controllers/userController.js
+++ controllers/userController.js
@@ -14,7 +14,7 @@ exports.mustBeLoggedIn = function (req, res, next) {
 exports.login = function (req, res) {
     let user = new User(req.body)
     user.login().then((result) => {
-        req.session.user = { avatar: user.avatar, username: user.data.username }
+        req.session.user = { avatar: user.avatar, username: user.data.username, _id: user.data._id }
         req.session.save(() => {
             res.redirect('/')
         })
@@ -36,7 +36,7 @@ exports.logout = (req, res) => {
 exports.register = function (req, res) {
     let user = new User(req.body)
     user.register().then(() => {
-        req.session.user = { username: user.data.username, avatar: user.avatar }
+        req.session.user = { username: user.data.username, avatar: user.avatar, _id: user.data._id }
         req.session.save(() => {
             res.redirect('/')
         })
