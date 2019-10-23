adb365b55668d56f3eb7a2765a0e0ed067dd4f4b
Gjoko <temelkovgjoko@gmail.com>
2019-10-23 23:40:46 +0200

    updating pots in database 1

--- controllers/postController.js
+++ controllers/postController.js
@@ -30,3 +30,32 @@ exports.viewEditScreen = async function (req, res) {
         res.render("404")
     }
 }
+
+exports.edit = function (req, res) {
+    let post = new Post(req.body, req.visitorId, req.params.id)
+    post.update().then((status) => {
+        // the post was succesfully updated in the database
+        // or user did have permission, but there were validation errors
+        if (status == "success") {
+            // post was updated in db
+            req.flash("success", "Post successfully updated.")
+            req.session.save(() => {
+                res.redirect(`/post/${req.params.id}/edit`)
+            })
+        } else {
+            post.errors.forEach(() => {
+                req.flash("errors", error)
+            })
+            req.session.save(() => {
+                res.redirect(`/post/${req.params.id}/edit`)
+            })
+        }
+    }).catch(() => {
+        // a post with the requested id doesn't exist
+        // or the current visitor is not the owner of the requested post
+        req.flash("errors", "You do not have permission to perform that action.")
+        req.session.save(() => {
+            res.redirect("/")
+        })
+    })
+}
