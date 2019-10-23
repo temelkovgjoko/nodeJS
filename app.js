e033e2d1b707a2897bbcffcc214f73904eb85246
Gjoko <temelkovgjoko@gmail.com>
2019-10-24 01:22:50 +0200

    markdown safe user generated html

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require('express')
 const session = require('express-session')
 const MongoStore = require('connect-mongo')(session)
 const flash = require('connect-flash')
+const markdown = require('marked')
 const app = express()
+const sanitizeHTML = require('sanitize-html')
 
 let sessionOptions = session({
     secret: "Javascript 123",
@@ -15,6 +17,11 @@ let sessionOptions = session({
 app.use(sessionOptions)
 app.use(flash())
 app.use(function (req, res, next) {
+    // make our markdown function available from within ejs template functions
+    res.locals.filterUserHTML = function (content) {
+        return sanitizeHTML(markdown(content), { allowTags: ['p', 'br', 'ul', 'ol', 'li', 'strong', 'bold', 'i', 'em', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6'], allowedAttributes: {} })
+    }
+
     // make all error and success flash messages available from all templates
     res.locals.errors = req.flash("errors")
     res.locals.success = req.flash("success")
