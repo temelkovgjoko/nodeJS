39acca58657613ad9947218733245af0daf9f78f
Gjoko <temelkovgjoko@gmail.com>
2019-10-25 17:00:08 +0200

    64

--- frontend-js/modules/search.js
+++ frontend-js/modules/search.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export default class Search {
-  // 1. Select Dom elements, and keep track of any useful data
+  // 1. Select DOM elements, and keep track of any useful data
   constructor() {
     this.injectHTML()
     this.headerSearchIcon = document.querySelector(".header-search-icon")
@@ -35,7 +35,7 @@ export default class Search {
       this.hideResultsArea()
     }
 
-    if (value != "" && this.value != this.previousValue) {
+    if (value != "" && value != this.previousValue) {
       clearTimeout(this.typingWaitTimer)
       this.showLoaderIcon()
       this.hideResultsArea()
@@ -46,31 +46,29 @@ export default class Search {
   }
 
   sendRequest() {
-    axios.post('/search', { searchTerm: this.inputField.value }).then(response => {
+    axios.post('/search', {searchTerm: this.inputField.value}).then(response => {
       console.log(response.data)
       this.renderResultsHTML(response.data)
     }).catch(() => {
-      alert("The request failed.")
+      alert("Hello, the request failed.")
     })
   }
 
   renderResultsHTML(posts) {
     if (posts.length) {
       this.resultsArea.innerHTML = `<div class="list-group shadow-sm">
-            <div class="list-group-item active"><strong>(${posts.length > 1 ? `${posts.length} items found` : '1 item found'})</div>
-            ${posts.map(post => {
+      <div class="list-group-item active"><strong>Search Results</strong> (${posts.length > 1 ? `${posts.length} items found` : '1 item found'})</div>
+      ${posts.map(post => {
         let postDate = new Date(post.createdDate)
         return `<a href="/post/${post._id}" class="list-group-item list-group-item-action">
         <img class="avatar-tiny" src="${post.author.avatar}"> <strong>${post.title}</strong>
         <span class="text-muted small">by ${post.author.username} on ${postDate.getMonth()}/${postDate.getDate()}/${postDate.getFullYear()}</span>
-        </a>`
+      </a>`
       }).join('')}
-      </div>`
+    </div>`
     } else {
-      this.resultsArea.innerHTML = `< p class="alert alert-danger text-center shadow-sm" > Sorry, we could nopt find any results for that search.</p>`
-
+      this.resultsArea.innerHTML = `<p class="alert alert-danger text-center shadow-sm">Sorry, we could not find any results for that search.</p>`
     }
-
     this.hideLoaderIcon()
     this.showResultsArea()
   }
@@ -83,10 +81,10 @@ export default class Search {
     this.loaderIcon.classList.remove("circle-loader--visible")
   }
 
-
   showResultsArea() {
     this.resultsArea.classList.add("live-search-results--visible")
   }
+
   hideResultsArea() {
     this.resultsArea.classList.remove("live-search-results--visible")
   }
@@ -100,24 +98,23 @@ export default class Search {
     this.overlay.classList.remove("search-overlay--visible")
   }
 
-  injectHTML() {
-    document.body.insertAdjacentHTML('beforeend', `< div class="search-overlay" >
-        <div class="search-overlay-top shadow-sm">
-          <div class="container container--narrow">
-            <label for="live-search-field" class="search-overlay-icon"><i class="fas fa-search"></i></label>
-            <input type="text" id="live-search-field" class="live-search-field" placeholder="What are you interested in?">
-              <span class="close-live-search"><i class="fas fa-times-circle"></i></span>
-          </div>
-          </div>
 
-          <div class="search-overlay-bottom">
-            <div class="container container--narrow py-3">
-              <div class="circle-loader"></div>
-            </div>
-          </div>
-        </div>
-      </div >
-        `)
+  injectHTML() {
+    document.body.insertAdjacentHTML('beforeend', `<div class="search-overlay">
+    <div class="search-overlay-top shadow-sm">
+      <div class="container container--narrow">
+        <label for="live-search-field" class="search-overlay-icon"><i class="fas fa-search"></i></label>
+        <input type="text" id="live-search-field" class="live-search-field" placeholder="What are you interested in?">
+        <span class="close-live-search"><i class="fas fa-times-circle"></i></span>
+      </div>
+    </div>
+
+    <div class="search-overlay-bottom">
+      <div class="container container--narrow py-3">
+        <div class="circle-loader"></div>
+        <div class="live-search-results"></div>
+      </div>
+    </div>
+  </div>`)
   }
 }
-
