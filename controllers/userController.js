37d743fa1264d5b8b78f26cfa33d7516a872201b
Gjoko <temelkovgjoko@gmail.com>
2019-10-28 11:39:12 +0100

    stop following a user 2

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,12 +2,16 @@ const User = require('../models/User')
 const Post = require('../models/Post')
 const Follow = require('../models/Follow')
 
+
 exports.sharedProfileData = async function (req, res, next) {
+    let isVisitorsProfile = false
     let isFollowing = false
     if (req.session.user) {
+        isVisitorsProfile = req.profileUser._id.equals(req.session.user._id)
         isFollowing = await Follow.isVisitorFollowing(req.profileUser._id, req.visitorId)
     }
 
+    req.isVisitorsProfile = isVisitorsProfile
     req.isFollowing = isFollowing
     next()
 }
@@ -87,7 +91,8 @@ exports.profilePostsScreen = function (req, res) {
             posts: posts,
             profileUsername: req.profileUser.username,
             profileAvatar: req.profileUser.avatar,
-            isFollowing: req.isFollowing
+            isFollowing: req.isFollowing,
+            isVisitorsProfile:  req.isVisitorsProfile
         })
     }).catch(function () {
         res.render("404")
