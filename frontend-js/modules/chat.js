2a366ff2b091528fc8a38d66ebb30e92d797075c
Gjoko <temelkovgjoko@gmail.com>
2019-10-28 22:37:38 +0100

    finishing chat 2

--- frontend-js/modules/chat.js
+++ frontend-js/modules/chat.js
@@ -1,3 +1,5 @@
+import DOMPurify from 'dompurify'
+
 export default class Chat {
     constructor() {
         this.openedYet = false
@@ -21,9 +23,19 @@ export default class Chat {
     }
 
     // Methods
-
     sendMessageToServer() {
         this.socket.emit('chatMessageFromBrowser', { message: this.chatField.value })
+        this.chatLog.insertAdjacentHTML('beforeend', DOMPurify.sanitize(`
+        <div class="chat-self">
+        <div class="chat-message">
+          <div class="chat-message-inner">
+            ${this.chatField.value}
+          </div>
+        </div>
+        <img class="chat-avatar avatar-tiny" src="${this.avatar}">
+      </div>
+        `))
+        this.chatLog.scrollTop = this.chatLog.scrollHeight
         this.chatField.value = ''
         this.chatField.focus()
     }
@@ -37,26 +49,31 @@ export default class Chat {
         }
         this.openedYet = true
         this.chatWrapper.classList.add("chat--visible")
+        this.chatField.focus()
     }
 
     openConnection() {
         this.socket = io()
+        this.socket.on('welcome', data => {
+            this.username = data.username
+            this.avatar = data.avatar
+        })
         this.socket.on('chatMessageFromServer', (data) => {
             this.displayMessageFromServer(data)
         })
     }
 
     displayMessageFromServer(data) {
-        this.chatLog.insertAdjacentHTML('beforeend', `
+        this.chatLog.insertAdjacentHTML('beforeend', DOMPurify.sanitize(`
         <div class="chat-other">
-        <a href="#"><img class="avatar-tiny" src="${data.avatar}"></a>
+        <a href="/profile/${data.username}"><img class="avatar-tiny" src="${data.avatar}"></a>
         <div class="chat-message"><div class="chat-message-inner">
-          <a href="#"><strong>${data.username}:</strong></a>
+          <a href="/profile/${data.username}"><strong>${data.username}:</strong></a>
           ${data.message}
         </div></div>
-      </div>`)
+      </div>`))
+        this.chatLog.scrollTop = this.chatLog.scrollHeight
     }
-
 
     injectHTML() {
         this.chatWrapper.innerHTML = `
