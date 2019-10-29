bf5ed115da099053e5177f8d6dc630b3782c9a0e
Gjoko <temelkovgjoko@gmail.com>
2019-10-29 16:03:37 +0100

    JWT /1

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,7 @@
 const User = require('../models/User')
 const Post = require('../models/Post')
 const Follow = require('../models/Follow')
-
+const jwt = require('jsonwebtoken')
 exports.doesUsernameExist = function (req, res) {
     User.findByUsername(req.body.username).then(function () {
         res.json(true)
@@ -66,7 +66,7 @@ exports.login = function (req, res) {
 exports.apiLogin = function (req, res) {
     let user = new User(req.body)
     user.login().then((result) => {
-        res.json("Good job, that is a real username and password")
+        res.json(jwt.sign({ _id: user.data._id }, process.env.JWTSECRET, { expiresIn: '7d' }))
     }).catch((e) => {
         res.json("Sorry, your values are not correct")
     })
